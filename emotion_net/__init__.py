679e8e4d6837b76a3640228462dab283b36cbf40
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2025-05-17 22:49:16 +0700

    Revamp

--- emotion_net/__init__.py
+++
@@ -1,3 +0,0 @@
-﻿"""EmotionNet package for emotion recognition."""
-
-__version__ = "0.1.0" 
--- emotion_net/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-﻿"""Utility functions for EmotionNet."""
-
-from .visualization import plot_confusion_matrix
-
-__all__ = ['plot_confusion_matrix'] 
